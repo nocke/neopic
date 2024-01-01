6a35c6e3f84e3f179b16c02346ddbd4e8ded5095
_USER_ <_@_>
2024-01-01 22:19:35 +0100

    vite.main.config to support actual build (parameterless) and vite test build

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,19 +1,22 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+// build params only for 'vite sanity testing' with `npm run vite:build-main`
+// for actual dev (npm start) and publishing this is a hindrance
 
-  build: {
-    rollupOptions: {
-      input: 'index.html',
-      output: {
-        entryFileNames: `main.js`,
-        chunkFileNames: `main.js`,
-        assetFileNames: `assets/main.[ext]`
-      }
-    },
-    outDir: '.vite/build'
+const build = process.env.npm_lifecycle_script?.includes('vite build') ? {
+  rollupOptions: {
+    input: 'src/main.ts',
+    output: {
+      entryFileNames: `main.js`,
+      chunkFileNames: `main.js`,
+      assetFileNames: `assets/main.[ext]`
+    }
   },
+  outDir: '.vite/build'
+} : undefined;
 
+export default defineConfig({
+  build,
   resolve: {
     // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
     browserField: false,
