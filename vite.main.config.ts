19a85d84f008f2807f5f8eecb39a9c83cf540127
_USER_ <_@_>
2024-01-02 12:07:19 +0100

    (possibly) actually working Solution for browserField browserField as unknown in type declaration, probably not doing what was promised

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -18,8 +18,11 @@ const build = process.env.npm_lifecycle_script?.includes('vite build') ? {
 export default defineConfig({
   build,
   resolve: {
-    // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
-    browserField: false,
     mainFields: ['module', 'jsnext:main', 'jsnext'],
   },
+  // optimizeDeps: {
+  //   // specify the libraries to be optimized for Node.js
+  //   include: ['axios'],
+  // }
+
 });
