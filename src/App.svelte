98abdf14aa458fd6222c366f2e5b2c4bdcc8107c
_USER_ <_@_>
2024-01-02 12:08:44 +0100

    WIP: Svelta added, compiles commeneted out, trouble enabling

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let name = 'world';
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+	border: 8px dashed purple;
+  }
+</style>
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,20 +1,22 @@
 import { defineConfig } from 'vite';
+// import svelte from '@sveltejs/vite-plugin-svelte/src/index.js';
 
-export default defineConfig({
-	plugins: [
-
-	],
-	build: {
-		rollupOptions: {
-		  input: 'src/renderer.ts',
-		  output: {
+const build = process.env.npm_lifecycle_script?.includes('vite build') ? {
+	rollupOptions: {
+		input: 'src/renderer.ts',
+		output: {
 			entryFileNames: `renderer.js`,
 			chunkFileNames: `renderer.js`,
 			assetFileNames: `assets/renderer.[ext]`
-		  }
-		},
-		outDir: '.vite/renderer/main_window'
-	  },
+		}
+	},
+	outDir: '.vite/renderer/main_window'
+} : undefined;
 
-
+export default defineConfig({
+	build,
+	plugins: [
+		// svelte()
+		// PROBLEM: This expression is not callable. 		Type 'typeof import("@sveltejs/vite-plugin-svelte")' has no call signatures.ts(2349)
+	]
 });
