2a06988e3df2898fee36d906718f1afd4e4fbcc8
_USER_ <_@_>
2024-01-04 21:48:15 +0100

    Svelte activated

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  let name = 'world';
+  let name = 'Svelte';
 </script>
 
 <main>
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,19 +1,19 @@
-// import App from './App.svelte';
+import App from './App.svelte';
 import './index.sass';
 
 console.log('👋 This message is being logged by "renderer.ts", included via Vite');
 console.log('howdy, renderer 345789');
 
 
-// const target = document.querySelector('main');
-// if (target) {
-//   const app = new App({
-//     target,
-//     props: {
-//       // Your props here
-//     }
-//   });
-// } else {
-//   console.error('No <main> element found to mount the Svelte app');
-// }
+const target = document.body;
+if (target) {
+  const app = new App({
+    target,
+    props: {
+      // Your props here
+    }
+  });
+} else {
+  console.error('No <main> element found to mount the Svelte app');
+}
 
