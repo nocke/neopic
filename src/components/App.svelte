8375bd6fe0a9562881110546bf168877762809ef
Frank Nocke <frank.nocke@sbb.ch>
2024-01-13 12:45:56 +0100

    eslint, ignore ts error on svelte

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,18 +1,24 @@
-<script>
-  const name = 'Svelte'
-
+<script lang="ts">
   import { onMount } from 'svelte'
-
   import Header from './Header.svelte'
 
+  const name = 'Svelte'
+
   onMount(async () => {
-    console.log('svelte mounted')
+      console.log('svelte mounted')
+    setTimeout(() => {
+      document.getElementById('listDirButton')?.click()
+    }, 1000)
   })
 
   async function listDir() {
-    console.log('list Directory (renderer)')
-    const files = await window.electron.ipcRenderer.invoke('list-home-dir')
-    console.log(JSON.stringify(files, null))
+    const joe: any = '12345'
+    let joeNumber: number = <number><unknown>joe
+    // test failure joeNumber = 'abc'
+
+    console.log('list Directory (renderer)', joe, joeNumber)
+    const files = await window.electron.ipcRenderer.invoke('list-home-dir');
+    console.log( files.join('\n') )
   }
 </script>
 
@@ -20,7 +26,7 @@
   <Header></Header>
   <h1>Hello {name} 123!</h1>
 
-  <button on:click={listDir}>
+  <button id="listDirButton" on:click={listDir}>
     List home contents
   </button>
 </main>
