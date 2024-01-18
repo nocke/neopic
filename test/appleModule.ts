3d4b766c84f2b113ab8077d4732eb380c5f5900b
Frank Nocke <frank.nocke@sbb.ch>
2024-01-18 21:40:24 +0100

    preliminary with appleModule (in test/)

---
+++ test/appleModule.ts
@@ -0,0 +1,7 @@
+export const sayHi: () => void = () => {
+  console.log('Hi apple!')
+}
+
+export const truth = 42
+
+export default { sayHi, truth }
--- test/selftest.spec.ts
+++ test/selftest.spec.ts
@@ -1,9 +1,13 @@
 import { expect } from 'chai'
+import dummyModule from './appleModule.js'
+// works better than   import dummyModule from '../src/dummyModule.js'
 
-console.log('███ selftest.spec.ts')
 
 describe('Self Test', () => {
   it('should assert that 1 + 1 equals 2', () => {
     expect(1 + 1).to.equal(2)
+  })
+  it('check on dummyModule', () => {
+    expect(dummyModule.truth).to.equal(42)
   })
 })
