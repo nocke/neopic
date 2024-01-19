5e352dc7e3fe8e6c6b749387bb80c4c94fa13f3d
Frank Nocke <frank.nocke@sbb.ch>
2024-01-19 10:33:36 +0100

    switch-to resp. add assert style (my preference)

--- test/selftest.spec.ts
+++ test/selftest.spec.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai'
+import { expect, assert } from 'chai'
 // works not      import dummyModule from '../src/dummyModule'
 // works better   import dummyModule from '../src/dummyModule.js'
 // works import dummyModule from './appleModule.js'
@@ -7,8 +7,10 @@ import dummyModule from '../src/dummyModule.js'
 describe('Self Test', () => {
   it('should assert that 1 + 1 equals 2', () => {
     expect(1 + 1).to.equal(2)
+    assert.strictEqual(1 + 1, 2)
   })
   it('check on dummyModule', () => {
     expect(dummyModule.truth).to.equal(42)
+    assert.strictEqual(dummyModule.truth, 42)
   })
 })
