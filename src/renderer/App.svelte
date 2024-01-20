2c04306b003f7dc256f2c9bf346569bd36ab7aad
Frank Nocke <frank.nocke@sbb.ch>
2024-01-20 21:07:17 +0100

    App.svlete: fix some types

--- src/renderer/App.svelte
+++ src/renderer/App.svelte
@@ -1,33 +1,29 @@
+<style src="./App.sass" lang="sass"></style>
+
 <script lang="ts">
-  import { onMount } from 'svelte'
-       import Header from './Header.svelte'
+import { onMount } from 'svelte'
+import Header from './Header.svelte'
 
-  const name = 'Svelte'
-  let files = []
+const name = 'Svelte'
+let files: string[] = []
 
-  onMount(async () => {
-    console.log('svelte mounted')
-    setTimeout(() => {
-      document.getElementById('listDirButton')?.click()
-    }, 500)
-  })
+onMount(async () => {
+  console.log('svelte mounted')
+  setTimeout(() => {
+    document.getElementById('listDirButton')?.click()
+  }, 500)
+})
 
-  async function listDir() {
-    let joe = 12345
-    joe = 'bert'
-
-    console.log('list Directory (renderer)', joe)
-    files = await window.electron.ipcRenderer.invoke('list-home-dir')
-  }
+async function listDir() {
+  files = await window.electron.ipcRenderer.invoke<string[]>('list-home-dir')
+}
 </script>
 
 <main>
   <Header></Header>
   <h1>Hello {name} 123!</h1>
 
-  <button id="listDirButton" on:click={listDir}>
-    List home contents 123
-  </button>
+  <button id="listDirButton" on:click="{listDir}"> List home contents 123 </button>
 
   <div id="files">
     {#each files as file (file)}
@@ -35,5 +31,3 @@
     {/each}
   </div>
 </main>
-
-<style src="./App.sass" lang="sass"></style>
