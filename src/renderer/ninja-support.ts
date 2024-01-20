c9487d3e00f131a4210c886b9eded5c6cbc62a55
Frank Nocke <frank.nocke@sbb.ch>
2024-01-20 22:17:38 +0100

    ninja hotkeys working \o/

---
+++ src/renderer/ninja-support.ts
@@ -0,0 +1,23 @@
+import { INinjaAction } from 'ninja-keys/dist/interfaces/ininja-action'
+
+// REF attributes: /github.com/ssleptsov/ninja-keys?tab=readme-ov-file#attributes
+export const hotkeys: INinjaAction[] = [
+  {
+    id: 'Home',
+    title: 'Open Home',
+    hotkey: 'cmd+h,ctrl+h',
+    mdIcon: 'home',
+    handler: () => {
+      console.log('navigation to home')
+    },
+  },
+  {
+    id: 'Open Projects',
+    title: 'Open Projects',
+    hotkey: 'cmd+p,ctrl+p',
+    mdIcon: 'apps',
+    handler: () => {
+      console.log('navigation to projects')
+    },
+  },
+]
