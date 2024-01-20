bb0ebaaef74f4b27a1bd04529473e35e4cf2ed91
Frank Nocke <frank.nocke@sbb.ch>
2024-01-20 22:17:56 +0100

    0.6.0 ninja hotkeys working \o/

--- src/renderer/App.svelte
+++ src/renderer/App.svelte
@@ -1,22 +1,33 @@
 <style src="./App.sass" lang="sass"></style>
 
 <script lang="ts">
-import { onMount } from 'svelte'
-import Header from './Header.svelte'
+  import { onMount } from 'svelte'
+  import Header from './Header.svelte'
+  import { NinjaKeys } from 'ninja-keys'
+  import { hotkeys } from './ninja-support'
 
-const name = 'Svelte'
-let files: string[] = []
+  const name = 'Svelte'
+  let files: string[] = []
 
-onMount(async () => {
-  console.log('svelte mounted')
-  setTimeout(() => {
-    document.getElementById('listDirButton')?.click()
-  }, 500)
-})
+  onMount(async () => {
+    console.log('svelte mounted')
 
-async function listDir() {
-  files = await window.electron.ipcRenderer.invoke<string[]>('list-home-dir')
-}
+    const ninja: NinjaKeys | null = document.querySelector('ninja-keys')
+    if (ninja) {
+      console.log('### ninja-keys found')
+      ninja.data = hotkeys
+    } else {
+      console.log('### ninja-keys NOT found')
+    }
+
+    setTimeout(() => {
+      document.getElementById('listDirButton')?.click()
+    }, 500)
+  })
+
+  async function listDir() {
+    files = await window.electron.ipcRenderer.invoke<string[]>('list-home-dir')
+  }
 </script>
 
 <main>
@@ -24,6 +35,9 @@ async function listDir() {
   <h1>Hello {name} 123!</h1>
 
   <button id="listDirButton" on:click="{listDir}"> List home contents 123 </button>
+
+  <b>Ctrl+P for command palette,  ctrl+h, ctrl+o for whatever</b>
+  <ninja-keys placeholder="Type a command or name…" openHotkey="cmd+p,cmd+shift+p,ctrl+p,ctrl+shift+p"></ninja-keys>
 
   <div id="files">
     {#each files as file (file)}
