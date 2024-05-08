8b44bed456b3470abe3acc43b59cfd86af8528a6
Frank Nocke <frank.nocke@sbb.ch>
2024-05-08 15:04:09 +0200

    adjust circle-ci script

--- script/script-helpers.js
+++ script/script-helpers.js
@@ -4,3 +4,7 @@ export const exec = (command, options = {}) => {
   console.log('exec:', command)
   return execSync(command, { encoding: 'utf8', ...options }).trim()
 }
+
+export const log = console.log
+export const warn = console.warn
+export const error = console.error
