84426a43c786cfb9fa1d781265bc615d29fe49a8
Frank Nocke <frank.nocke@sbb.ch>
2024-05-12 21:26:44 +0200

    main: right-align window, prepare ipc tests

---
+++ src/main/handlers/ipcHandlers.ts
@@ -0,0 +1,44 @@
+import { ipcMain } from 'electron'
+
+import fs from 'fs'
+import os from 'os'
+import { DirectoryError, FileList } from '../../shared/sharedTypes'
+
+function isErrnoException(e: unknown): e is NodeJS.ErrnoException {
+  if ('code' in (e as any)) return true
+  else return false
+}
+
+export const setupIPC = () => {
+
+  ipcMain.handle('get-home-dir', async (): Promise<string> => {
+    return os.homedir()
+  })
+
+  ipcMain.handle('list-dir', async (_event, dirPath: string): Promise<FileList> => {
+    console.log('render: list-dir 7', dirPath)
+
+    try {
+      await fs.promises.access(dirPath, fs.constants.F_OK)
+      await fs.promises.access(dirPath, fs.constants.R_OK)
+      const files = fs.promises.readdir(dirPath)
+      return files
+    } catch (err: unknown) {
+      if (err && isErrnoException(err) && err.code === 'ENOENT') {
+        return 'ENOENT' as DirectoryError
+      } else if (err && isErrnoException(err) && err.code === 'EACCESS') {
+        return 'EACCESS' as DirectoryError
+      } else {
+        throw err
+      }
+    }
+  })
+
+  // ipcMain.handle('get-settings', async (_event): Promise<Settings> => {
+  //   return {
+  //     theme: 'banana',
+  //     language: 'en'
+  //   }
+  // })
+
+}
--- src/main/main.ts
+++ src/main/main.ts
@@ -1,21 +1,15 @@
-import { app, BrowserWindow, ipcMain } from 'electron'
+import { app, BrowserWindow, screen } from 'electron'
 import path from 'path'
 
-import fs from 'fs'
-import os from 'os'
-import { DirectoryError, FileList } from '../shared/sharedTypes'
+import { setupIPC } from './handlers/ipcHandlers'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit()
 }
 
-function isErrnoException(e: unknown): e is NodeJS.ErrnoException {
-  if ('code' in (e as any)) return true
-  else return false
-}
-
-const onReady = () => {
+app.on('ready', () => {
+  setupIPC()
 
   const args = process.argv.slice(1)
   if (args.includes('-selftest')) {
@@ -24,17 +18,24 @@ const onReady = () => {
       app.exit(0) // 0 := all good
       console.log('Done\nNow')
     }, 3000)
-
-    // COULDDO return; // Skip creating the window
+    // COULDDO skip creating the window by calling 'return'
   }
 
+  const allDisplays = screen.getAllDisplays()
+  const rightmostDisplay = allDisplays.reduce((prev, curr) => {
+    return (curr.bounds.x > prev.bounds.x) ? curr : prev;
+  });
+
+  // align left half of rightmost display
   const mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    x: rightmostDisplay.bounds.x,
+    y: rightmostDisplay.bounds.y,
+    width: rightmostDisplay.size.width / 2,
+    height: rightmostDisplay.size.height,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
-  })
+  });
 
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
@@ -42,44 +43,9 @@ const onReady = () => {
     mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`))
   }
 
-  // ████████████████████████████████████████████████████████████
-
-  // ipcMain.handle('get-settings', async (_event): Promise<Settings> => {
-  //   return {
-  //     theme: 'banana',
-  //     language: 'en'
-  //   }
-  // })
-
-  ipcMain.handle('get-home-dir', async (_event): Promise<string> => {
-    return os.homedir()
-  })
-
-  ipcMain.handle('list-dir', async (_event, dirPath: string): Promise<FileList> => {
-    console.log('render: list-dir 7', dirPath)
-
-    try {
-      await fs.promises.access(dirPath, fs.constants.F_OK)
-      await fs.promises.access(dirPath, fs.constants.R_OK)
-      const files = fs.promises.readdir(dirPath)
-      return files
-    } catch (err: unknown) {
-      if (err && isErrnoException(err) && err.code === 'ENOENT') {
-        return 'ENOENT' as DirectoryError
-      } else if (err && isErrnoException(err) && err.code === 'EACCESS') {
-        return 'EACCESS' as DirectoryError
-      } else {
-        throw err
-      }
-    }
-  })
-
-  // ████████████████████████████████████████████████████████████
-
   mainWindow.webContents.openDevTools()
-} // onReady
+}) // onReady
 
-app.on('ready', onReady)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
