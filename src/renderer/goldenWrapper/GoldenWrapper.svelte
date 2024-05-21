baaadb32e610aeb130028d6872d4bfc1da522acd
Frank Nocke <frank.nocke@sbb.ch>
2024-05-21 16:20:50 +0200

    gold: component registration

--- src/renderer/goldenWrapper/GoldenWrapper.svelte
+++ src/renderer/goldenWrapper/GoldenWrapper.svelte
@@ -2,7 +2,7 @@
 <style src="./GoldenWrapper.sass" lang="sass"></style>
 
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte'
+  import { onMount, onDestroy, SvelteComponent } from 'svelte'
   import { VirtualLayout, LayoutConfig, ComponentContainer, ResolvedComponentItemConfig } from 'golden-layout'
   import Test from '../test/Test.svelte'
 
@@ -22,36 +22,48 @@
     },
   }
 
+  const svelteComponents = new Map<ComponentContainer, SvelteComponent>()
+
   function handleBindComponentEvent(container: ComponentContainer, itemConfig: ResolvedComponentItemConfig) {
     const { componentState } = itemConfig
-    new Test({
+    const component = new Test({
       target: container.element,
       props: componentState,
     })
+    svelteComponents.set(container, component)
     return {
-      component: undefined,
+      component,
       virtual: true,
     }
   }
 
   function handleUnbindComponentEvent(container: ComponentContainer) {
-    // Optional: clean up if necessary
+    const component = svelteComponents.get(container)
+    if (component) {
+      component.$destroy()
+      svelteComponents.delete(container)
+    }
+  }
+
+  function updateLayoutSize() {
+    if (container) {
+      const width = container.offsetWidth
+      const height =  window.innerHeight / 2 // matches the height:50% in sass
+      virtualLayout?.setSize(width, height)
+    }
   }
 
   onMount(() => {
     virtualLayout = new VirtualLayout(container, handleBindComponentEvent, handleUnbindComponentEvent)
     virtualLayout.loadLayout(config)
+    window.addEventListener('resize', updateLayoutSize)
+    updateLayoutSize() // Initial size update
   })
 
   onDestroy(() => {
+    window.removeEventListener('resize', updateLayoutSize)
     virtualLayout?.destroy()
   })
 </script>
 
-<svelte:window
-  on:resize="{() => {
-    console.log('██ on:resize', container.offsetWidth, container.offsetHeight)
-    virtualLayout?.setSize(container.offsetWidth, window.innerHeight / 2) // matches the height:50% in sass
-  }}"
-/>
 <div bind:this="{container}" class="golden-container"></div>
