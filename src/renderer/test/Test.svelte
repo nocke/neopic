b53be9e159e1122cefa2f42517177c0339a2c584
Frank Nocke <frank.nocke@sbb.ch>
2024-05-21 21:36:01 +0200

    golden: add {message} to test

--- src/renderer/test/Test.svelte
+++ src/renderer/test/Test.svelte
@@ -1,6 +1,7 @@
-<section>
-  Test, Test, Test, Test, Test, Test, Test, Test, Test, Test,
-  Test, Test, Test, Test, Test, Test, Test, Test, Test, Test
-</section>
-
 <style src="./Test.sass" lang="sass"></style>
+
+<script lang="ts">
+  export let message = 'ja, some default message'
+</script>
+
+<section>{message} Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test</section>
