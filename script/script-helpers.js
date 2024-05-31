2a221b937709f9b9cc6170e0db389dafaac2f3e4
Frank Nocke <frank.nocke@sbb.ch>
2024-05-31 23:08:37 +0200

    minor script improvements. ensure(), obey `remoteRepo`

--- script/script-helpers.js
+++ script/script-helpers.js
@@ -5,6 +5,16 @@ export const exec = (command, options = {}) => {
   return execSync(command, { encoding: 'utf8', ...options }).trim()
 }
 
+// conditional fails, but with a message
+export const ensure = (cond, ...message) => {
+  if (!cond) {
+    console.error(message)
+    process.exit(1)
+  }
+  return true // all good
+}
+
+
 export const log = console.log
 export const warn = console.warn
 export const errLog = console.error // `error` to collision-prone
