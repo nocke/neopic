72aaa06f3254bdbb001a1882b5e855802307ce3f
Frank Nocke <frank.nocke@sbb.ch>
2024-06-03 10:07:48 +0200

    publishing releases on github • release-* tags, `npm run release` • minimal release notes with download advice • circle ci routines for mock building • script-helpers improvements

--- script/script-helpers.js
+++ script/script-helpers.js
@@ -2,19 +2,18 @@ import { execSync } from 'child_process'
 
 export const exec = (command, options = {}) => {
   console.log('exec:', command)
-  return execSync(command, { encoding: 'utf8', ...options }).trim()
+  return execSync(command, { encoding: 'utf8', ...options }).toString().trim()
 }
 
 // conditional fails, but with a message
 export const ensure = (cond, ...message) => {
   if (!cond) {
-    console.error(message)
+    console.error('ensure() FAILED:', ...message)
     process.exit(1)
   }
   return true // all good
 }
 
-
 export const log = console.log
 export const warn = console.warn
-export const errLog = console.error // `error` to collision-prone
+export const errLog = console.error // `error` too collision-prone for a shortcut
